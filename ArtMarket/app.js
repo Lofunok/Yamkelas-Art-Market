dcd1945d06c0e7a3daaf8e0cced7876db286c225
Lofuno Kgotlagomang <lofunok@gmail.com>
2023-11-04 16:46:55 +0200

    Fixing my User API

--- ArtMarket/app.js
+++ ArtMarket/app.js
@@ -1,30 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/Users/index');
-var usersRouter = require('./routes/Users/users');
-var userRouter = require('./routes/Users/user');
-const transactionsRouter = require("./routes/Transactions/transactions");
-
+var indexRouter = require("./routes/Users/index");
+var usersRouter = require("./routes/Users/users");
+var userRouter = require("./routes/User/user");
+var transactionsRouter = require("./routes/Transactions/transactions");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/user', usersRouter);
-app.use('/transactions', transactionsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/user", userRouter);
+app.use("/transactions", transactionsRouter);
 
-app.listen(5000, ()=>{
-    console.log('server is listening')
+app.listen(5000, () => {
+  console.log("server is listening");
 });
-
 
 module.exports = app;
