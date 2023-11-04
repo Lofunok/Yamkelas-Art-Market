21ae7627ec33c03a7f6a1c9f1cbc8df3d580e6da
Lofuno Kgotlagomang <lofunok@gmail.com>
2023-11-04 20:53:07 +0200

    Extending User API Functionality

--- ArtMarket/routes/User/user.js
+++ ArtMarket/routes/User/user.js
@@ -47,4 +47,58 @@ router.post("/Createuser", function (req, res, next) {
   }
 });
 
+// Find specific user
+router.get("/Finduser", function (req, res, next) {
+  try {
+    var sql = "select * from users where username=? and password=?";
+
+    var values = [
+      req.body.hasOwnProperty("username") ? req.body.username : "",
+      req.body.hasOwnProperty("password") ? req.body.password : "",
+    ];
+
+    con.query(sql, values, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+
+      res.json({
+        status: "found",
+      });
+    });
+  } catch (e) {
+    res.status(404);
+    throw Error("Invalid JSON");
+  }
+});
+
+//update a user
+router.post("/Updateuser", function (req, res, next) {
+  try {
+    var sql =
+      "update product set name=?, surname=?, username=?, password=?, age=?, email=?, bio=?, phonenumber=?";
+
+    var values = [
+      req.body.hasOwnProperty("name") ? req.body.name : "",
+      req.body.hasOwnProperty("surname") ? req.body.surname : "",
+      req.body.hasOwnProperty("username") ? req.body.username : "",
+      req.body.hasOwnProperty("password") ? req.body.password : "",
+      req.body.hasOwnProperty("age") ? req.body.age : 0,
+      req.body.hasOwnProperty("email") ? req.body.email : "",
+      req.body.hasOwnProperty("bio") ? req.body.bio : "",
+      req.body.hasOwnProperty("phonenumber") ? req.body.phonenumber : "",
+    ];
+
+    con.query(sql, values, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+
+      res.json({
+        status: "created",
+      });
+    });
+  } catch (e) {
+    res.status(404);
+    throw Error("Invalid JSON");
+  }
+});
 module.exports = router;
